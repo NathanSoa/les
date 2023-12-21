5790cc385097038c2980243985f6df075e942c05
Nathan Soares <nathansoares809@gmail.com>
2023-12-21 11:25:30 -0300

    refactor: validator layer now return a map with error messages

--- backend/src/main/java/br/com/nathan/ecommerce/main/core/interfaces/Validator.java
+++ backend/src/main/java/br/com/nathan/ecommerce/main/core/interfaces/Validator.java
@@ -1,6 +1,9 @@
 package br.com.nathan.ecommerce.main.core.interfaces;
 
+import java.util.List;
+import java.util.Map;
+
 public interface Validator<T> {
 
-        void validate(T object);
+        Map<String, List<String>> validate(T object);
 }
