254b775c7fc447e8a3b7caeb5953665281685de2
Nathan Soares <nathansoares809@gmail.com>
2023-12-21 11:21:32 -0300

    feat: add new overload resolve method

--- backend/src/main/java/br/com/nathan/ecommerce/main/core/exceptions/MessageResolver.java
+++ backend/src/main/java/br/com/nathan/ecommerce/main/core/exceptions/MessageResolver.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Locale;
 
 @Component
@@ -14,5 +15,21 @@ public class MessageResolver {
 
     public String resolve(String code, Locale locale) {
         return messageSource.getMessage(code, null, locale);
+    }
+
+    public String resolve(String code) {
+        return messageSource.getMessage(code, null, Locale.getDefault());
+    }
+
+    public List<String> resolve(List<String> codes) {
+        return codes.stream()
+            .map(code -> messageSource.getMessage(code, null, Locale.getDefault()))
+            .toList();
+    }
+
+    public List<String> resolve(List<String> codes, Locale locale) {
+        return codes.stream()
+            .map(code -> messageSource.getMessage(code, null, locale))
+            .toList();
     }
 }
