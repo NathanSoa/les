528cdba734e7ba85595426f54b157a9a38ab9b55
Nathan Soares <nathansoares809@gmail.com>
2023-12-26 10:54:00 -0300

    feat: add company logo and base nav section

--- frontend/src/app/ui/dashboard/side-nav.tsx
+++ frontend/src/app/ui/dashboard/side-nav.tsx
@@ -1,7 +1,21 @@
+import CompanyLogo from '@/app/ui/company-logo'
+import NavItem from '@/app/ui/nav-item'
+
+import { HomeIcon } from 'lucide-react'
+
 export default function SideNav() {
   return (
-    <div className="bg-blue-theme h-full rounded-3xl p-6">
-      My sidebar goes here
+    <div className="h-full rounded-3xl bg-blue-theme p-6">
+      <div className="flex justify-center gap-2 pt-3 text-2xl">
+        <CompanyLogo size={32} />
+        <span className="text-zinc-100">MyCompany</span>
+      </div>
+
+      <nav className="pt-16">
+        <ul className="flex flex-col items-center gap-5">
+          <NavItem href="/dashboard" text="Dashboard" icon={HomeIcon} />
+        </ul>
+      </nav>
     </div>
   )
 }
