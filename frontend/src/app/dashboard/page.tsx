2eeb7e98076d811b587d690f232cb15681f8e378
Nathan Soares <nathansoares809@gmail.com>
2023-12-26 16:52:56 -0300

    feat: add main page title

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,3 +1,18 @@
+import clsx from 'clsx'
+import { Roboto } from 'next/font/google'
+
+const inter = Roboto({
+  weight: '700',
+  subsets: ['latin'],
+})
+
 export default function Dashboard() {
-  return <main>Dashboard</main>
+  return (
+    <h1 className="flex flex-col gap-1 text-xl">
+      Welcome back,{' '}
+      <strong className={clsx('text-4xl', true && inter.className)}>
+        Nathan Soares
+      </strong>
+    </h1>
+  )
 }
