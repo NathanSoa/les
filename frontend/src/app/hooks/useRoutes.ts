088318b8f58070e4f73c0155308cc2fdf1ab3d72
Nathan Soares <nathansoares809@gmail.com>
2023-12-26 15:48:23 -0300

    refactor: used getRoutes

--- frontend/src/app/hooks/useRoutes.ts
+++ frontend/src/app/hooks/useRoutes.ts
@@ -1,23 +1,39 @@
-import { HouseLine, User } from 'phosphor-react'
-import { ElementType } from 'react'
+import { Airplay, Gear, HouseLine, User } from 'phosphor-react'
+import { ElementType, useEffect, useState } from 'react'
 
+import { getRoutes } from '@/app/server/getRoutes'
+import { Base } from '@/app/server/Base'
+
+interface Route {
+  path: string
+  name: string
+}
 interface Path {
   path: string
   icon: ElementType
   label: string
 }
 
+const IconData = {
+  '/dashboard': HouseLine,
+  '/dashboard/customers': User,
+  '/dashboard/settings': Gear,
+  fallback: Airplay,
+}
+
+const settingsAsLast = (a: Route, b: Route) =>
+  a.name === 'Settings' ? 1 : b.name === 'Settings' ? -1 : 0
+
 export const useRoutes = (): Path[] => {
-  return [
-    {
-      path: '/dashboard',
-      icon: HouseLine,
-      label: 'Dashboard',
-    },
-    {
-      path: '/dashboard/customers',
-      icon: User,
-      label: 'Customers',
-    },
-  ]
+  const [routes, setRoutes] = useState<Route[]>([])
+
+  useEffect(() => {
+    getRoutes(Base.SRC).then((data) => setRoutes(data))
+  }, [])
+
+  return routes.sort(settingsAsLast).map((route) => ({
+    path: route.path,
+    icon: IconData[route.path as keyof typeof IconData] || IconData.fallback,
+    label: route.name,
+  }))
 }
