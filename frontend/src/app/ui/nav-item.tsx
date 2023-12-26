ccc4fb86fc4b3b5b51a57490b5d1a888d1d22fa7
Nathan Soares <nathansoares809@gmail.com>
2023-12-26 11:47:05 -0300

    feat: add conditional styling and Link element

--- frontend/src/app/ui/nav-item.tsx
+++ frontend/src/app/ui/nav-item.tsx
@@ -1,19 +1,31 @@
+'use client'
+
 import { usePathname } from 'next/navigation'
-import { ElementType } from 'react'
+import { ReactNode } from 'react'
+import { clsx } from 'clsx'
+import Link from 'next/link'
 
 interface NavItemProps {
   href: string
   text: string
-  icon: ElementType
+  icon: ReactNode
 }
 
 export default function NavItem({ href, text, icon: Icon }: NavItemProps) {
   const pathname = usePathname()
+  const isActualTab = href === pathname
+  const liClassName = isActualTab ? 'bg-white text-zinc-700' : 'text-zinc-50'
+  const iconClassName = isActualTab ? 'text-blue-theme' : 'text-zinc-50'
 
   return (
-    <li className="flex gap-5 text-zinc-50">
-      <Icon />
-      <a href={href}>{text}</a>
+    <li
+      className={clsx(
+        'flex w-full items-center justify-center gap-4 rounded-lg p-3 font-bold',
+        liClassName,
+      )}
+    >
+      <span className={clsx('text-3xl', iconClassName)}>{Icon}</span>
+      <Link href={href}>{text}</Link>
     </li>
   )
 }
