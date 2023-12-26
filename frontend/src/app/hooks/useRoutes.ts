59fd922f694e6b9bc3b5d0764bda8c676c1fd7ab
Nathan Soares <nathansoares809@gmail.com>
2023-12-26 13:19:27 -0300

    feat: create custom hook to retrieve application routes

---
+++ frontend/src/app/hooks/useRoutes.ts
@@ -0,0 +1,23 @@
+import { HouseLine, User } from 'phosphor-react'
+import { ElementType } from 'react'
+
+interface Path {
+  path: string
+  icon: ElementType
+  label: string
+}
+
+export const useRoutes = (): Path[] => {
+  return [
+    {
+      path: '/dashboard',
+      icon: HouseLine,
+      label: 'Dashboard',
+    },
+    {
+      path: '/customers',
+      icon: User,
+      label: 'Customers',
+    },
+  ]
+}
