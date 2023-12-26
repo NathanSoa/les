06c5631592ec6655321dfd1d7cf6abc8d8e554b3
Nathan Soares <nathansoares809@gmail.com>
2023-12-26 13:19:45 -0300

    refactor: used custom hook to render nav items

--- frontend/src/app/ui/dashboard/side-nav.tsx
+++ frontend/src/app/ui/dashboard/side-nav.tsx
@@ -1,11 +1,12 @@
 'use client'
 
+import { useRoutes } from '@/app/hooks/useRoutes'
 import CompanyLogo from '@/app/ui/company-logo'
 import NavItem from '@/app/ui/dashboard/nav-item'
 
-import { HouseLine, User } from 'phosphor-react'
-
 export default function SideNav() {
+  const routes = useRoutes()
+
   return (
     <div className="h-full rounded-3xl bg-blue-theme p-3">
       <div className="flex justify-center gap-2 pt-3 text-2xl">
@@ -15,8 +16,9 @@ export default function SideNav() {
 
       <nav className="pt-16">
         <ul className="flex w-full flex-col items-center gap-5">
-          <NavItem href="/dashboard" text="Dashboard" icon={<HouseLine />} />
-          <NavItem href="/customers" text="Customers" icon={<User />} />
+          {routes.map(({ path, label, icon: Icon }) => (
+            <NavItem key={path} href={path} text={label} icon={<Icon />} />
+          ))}
         </ul>
       </nav>
     </div>
