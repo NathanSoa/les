4e0943646f1eecca8691a5910bf70687973f1963
Nathan Soares <nathansoares809@gmail.com>
2023-12-27 15:28:37 -0300

    feat: create aside section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import { Metadata } from 'next'
 import { Roboto_Serif as RobotoSerif } from 'next/font/google'
 
 const roboto = RobotoSerif({
-  weight: '700',
+  weight: '500',
   subsets: ['latin'],
 })
 
@@ -14,16 +14,21 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="grid-cols-half grid">
-      <div className="h-screen bg-gradient-to-b from-blue-theme to-sky-400">
-        <h1
-          className={clsx('p-10 font-semibold text-zinc-50', roboto.className)}
-        >
+      <aside className="h-screen bg-gradient-to-b from-sky-400 to-blue-theme text-zinc-50">
+        <h1 className={clsx('p-10 font-medium', roboto.className)}>
           MyCompany
         </h1>
-        Nice to see you again
-        <div>WELCOME BACK</div>
-      </div>
-      <div>Login Account</div>
+        <div className="mt-36 flex flex-col items-center">
+          <span className="pb-3 text-lg text-zinc-200">
+            Nice to see you again
+          </span>
+          <span className="flex flex-col items-center text-7xl">
+            WELCOME BACK
+            <div className="mt-4 h-2 w-16 rounded-full bg-white" />
+          </span>
+        </div>
+      </aside>
+      <main>Login Account</main>
     </div>
   )
 }
