e907e77e392fad79532fb936fb07eb569e39b021
Nathan Soares <nathansoares809@gmail.com>
2023-12-27 14:22:31 -0300

    fix: fixed wrong name

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx'
 import { Roboto } from 'next/font/google'
 
-const inter = Roboto({
+const roboto = Roboto({
   weight: '700',
   subsets: ['latin'],
 })
@@ -10,7 +10,7 @@ export default function Dashboard() {
   return (
     <h1 className="flex flex-col gap-1 text-xl">
       Welcome back,{' '}
-      <strong className={clsx('text-4xl', true && inter.className)}>
+      <strong className={clsx('text-4xl', roboto.className)}>
         Nathan Soares
       </strong>
     </h1>
