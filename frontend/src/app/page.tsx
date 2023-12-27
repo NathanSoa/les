038281e5f6556a33fa0120bed72ec41035af9ff8
Nathan Soares <nathansoares809@gmail.com>
2023-12-27 14:22:43 -0300

    feat: create base division

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,29 @@
+import clsx from 'clsx'
+import { Metadata } from 'next'
+import { Roboto_Serif as RobotoSerif } from 'next/font/google'
+
+const roboto = RobotoSerif({
+  weight: '700',
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: 'Login | MyCompany',
+}
+
 export default function Home() {
-  return <div>Hello World</div>
+  return (
+    <div className="grid-cols-half grid">
+      <div className="h-screen bg-gradient-to-b from-blue-theme to-sky-400">
+        <h1
+          className={clsx('p-10 font-semibold text-zinc-50', roboto.className)}
+        >
+          MyCompany
+        </h1>
+        Nice to see you again
+        <div>WELCOME BACK</div>
+      </div>
+      <div>Login Account</div>
+    </div>
+  )
 }
