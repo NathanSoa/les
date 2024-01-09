fa906ff0f58e5687be7098497cca2aaa6c4b5559
Nathan Soares <nathansoares809@gmail.com>
2024-01-09 09:39:53 -0300

    feat: add create Modal

--- frontend/src/app/ui/dashboard/customers/customers-header.tsx
+++ frontend/src/app/ui/dashboard/customers/customers-header.tsx
@@ -1,10 +1,17 @@
 'use client'
 
 import { UserPlus } from '@phosphor-icons/react'
+
+import { useModal } from '@/app/hooks/useModal'
+
 import Breadcrumb from '@/app/ui/breadcrumb'
 import Button from '@/app/ui/button'
+import Modal from '@/app/ui/modal'
+import Form from '@/app/ui/form'
 
 export default function CustomersHeader() {
+  const { isOpen, openModal, closeModal } = useModal()
+
   return (
     <>
       <Breadcrumb pageName={'Customers'} homePage="/dashboard" />
@@ -12,12 +19,17 @@ export default function CustomersHeader() {
         <h1 className="pb-6 pt-2 text-4xl font-bold">Customers</h1>
         <Button.Root
           className="m-2 flex items-center justify-center gap-2 border-blue-theme px-4 py-1 text-blue-theme hover:bg-blue-800 hover:text-white"
-          onClick={() => console.log('asdjkfajsdfjasfasjsdkf')}
+          onClick={() => openModal()}
         >
           <Button.Icon icon={<UserPlus size={32} />} />
           <Button.Label label="Create customer" />
         </Button.Root>
       </div>
+      {isOpen && (
+        <Modal title="MyModal" onHide={() => closeModal()}>
+          <Form>Hello Modal</Form>
+        </Modal>
+      )}
     </>
   )
 }
