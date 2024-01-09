67a8c868ec165b73bff88d8d8efe458f79290d03
Nathan Soares <nathansoares809@gmail.com>
2024-01-09 14:08:05 -0300

    refactor: change input component to forward ref

--- frontend/src/app/ui/input.tsx
+++ frontend/src/app/ui/input.tsx
@@ -1,16 +1,23 @@
-import { InputHTMLAttributes } from 'react'
+import { InputHTMLAttributes, forwardRef } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 type InputProps = InputHTMLAttributes<HTMLInputElement>
 
-export default function Input(props: InputProps) {
-  return (
-    <input
-      {...props}
-      className={twMerge(
-        props.className,
-        'h-7 w-full rounded-lg border border-gray-400 p-2',
-      )}
-    />
-  )
-}
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (props: InputProps, ref) => {
+    return (
+      <input
+        {...props}
+        className={twMerge(
+          props.className,
+          'h-7 w-full rounded-lg border border-gray-400 p-2',
+        )}
+        ref={ref}
+      />
+    )
+  },
+)
+
+Input.displayName = 'Input'
+
+export default Input
