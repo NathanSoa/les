bab9ecd968cacc506a3a041858ec569ff459a083
Nathan Soares <nathansoares809@gmail.com>
2024-01-09 15:42:43 -0300

    refactor: change hook to not repeat past two colors

--- frontend/src/app/hooks/useRandomColor.ts
+++ frontend/src/app/hooks/useRandomColor.ts
@@ -1,22 +1,41 @@
+const colors = [
+  'bg-red-500',
+  'bg-blue-500',
+  'bg-green-500',
+  'bg-yellow-500',
+  'bg-purple-500',
+  'bg-orange-500',
+  'bg-teal-500',
+  'bg-fuchsia-500',
+]
+
+const lastSelectedColorIndex: number[] = []
+
 export function useRandomColor() {
-  const colors = [
-    'bg-red-500',
-    'bg-blue-500',
-    'bg-green-500',
-    'bg-yellow-500',
-    'bg-purple-500',
-  ]
-
-  let lastSelectedColorIndex = -1
-
   function getColorAsBackground() {
-    let randomIndex = Math.floor(Math.random() * colors.length)
-    while (randomIndex === lastSelectedColorIndex) {
-      randomIndex = Math.floor(Math.random() * colors.length)
+    let randomIndex = getRandomIndex()
+
+    while (isPastTwoColor(randomIndex)) {
+      randomIndex = getRandomIndex()
     }
-    lastSelectedColorIndex = randomIndex
+
+    addColorToLastSelected(randomIndex)
+
     return colors[randomIndex]
   }
 
   return { getColorAsBackground }
+}
+
+const isPastTwoColor = (index: number) =>
+  lastSelectedColorIndex.find((i) => i === index) !== undefined
+
+const getRandomIndex = () => Math.floor(Math.random() * colors.length)
+
+const addColorToLastSelected = (index: number) => {
+  lastSelectedColorIndex.push(index)
+
+  if (lastSelectedColorIndex.length > 2) {
+    lastSelectedColorIndex.shift()
+  }
 }
