245a871aeddef5ba265e1af8af07e1ebf205b5c5
Nathan Soares <nathansoares809@gmail.com>
2024-01-09 14:14:49 -0300

    feat: create base types

---
+++ frontend/src/app/domain/customer/entity.ts
@@ -0,0 +1,61 @@
+import { BaseEntity } from '@/app/domain/common/base-entity'
+
+export interface Address {
+  streetPurpose: string[]
+  streetType: string
+  street: string
+  number: string
+  neighborhood: string
+  city: string
+  state: string
+  country: string
+  zipCode: string
+  alias: string
+  complement: string
+}
+
+export interface Card {
+  type: string
+  name: string
+  number: string
+  securityCode: string
+  flag: string
+  main: boolean
+}
+
+export interface Customer extends BaseEntity {
+  name: string
+  cpf: string
+  birthdate: string
+  email: string
+  password: string
+  phone: string
+  address: Address[]
+  cards: Card[]
+}
+
+export interface CustomerCreate {
+  name: string
+  cpf: string
+  birthdate: string
+  email: string
+  password: string
+  passwordConfirm: string
+  phone: string
+  ddd: string
+  address: Address[]
+  cards: Card[]
+}
+
+export const EmptyCustomer: Customer = {
+  id: '',
+  name: '',
+  cpf: '',
+  birthdate: '',
+  email: '',
+  password: '',
+  phone: '',
+  address: [],
+  cards: [],
+  active: true,
+}
