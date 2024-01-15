43adb6329b1574f65fcc89efbf73869e6b5c1d0c
Nathan Soares <nathansoares809@gmail.com>
2024-01-15 15:51:58 -0300

    feat: extract verifyTab to a function

--- frontend/src/app/hooks/useTabStyle.ts
+++ frontend/src/app/hooks/useTabStyle.ts
@@ -4,7 +4,7 @@ import { usePathname } from 'next/navigation'
 
 export function useTabStyle(href: string) {
   const pathname = usePathname()
-  const isActualTab = href === pathname
+  const isActualTab = verifyActualTab(href, pathname)
   const liClassName = isActualTab ? 'bg-white text-zinc-700' : 'text-zinc-50'
   const iconClassName = isActualTab ? 'text-blue-theme' : 'text-zinc-50'
 
@@ -12,4 +12,8 @@ export function useTabStyle(href: string) {
     liClassName,
     iconClassName,
   }
+}
+
+function verifyActualTab(href: string, pathname: string) {
+  return href === pathname || pathname.startsWith(`${href}/create`)
 }
