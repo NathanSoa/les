b67f84cc416d4362446475b178b70802e84a0078
Nathan Soares <nathansoares809@gmail.com>
2024-01-15 16:05:56 -0300

    refactor: change background color

--- frontend/src/app/ui/info-card.tsx
+++ frontend/src/app/ui/info-card.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 
 export default function InfoCard({ children }: { children: ReactNode }) {
   return (
-    <div className="mb-2 flex flex-col gap-1 border-2 bg-neutral-100 p-2">
+    <div className="mb-2 flex flex-col gap-1 border-2 bg-slate-50 p-2">
       {children}
     </div>
   )
