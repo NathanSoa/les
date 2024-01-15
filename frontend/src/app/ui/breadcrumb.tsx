97039f617150a5ee9bb2fd384cdc6ede110a79f4
Nathan Soares <nathansoares809@gmail.com>
2024-01-15 15:50:27 -0300

    refactor: change Breadcrumb  component to support multiples past pages

--- frontend/src/app/ui/breadcrumb.tsx
+++ frontend/src/app/ui/breadcrumb.tsx
@@ -1,22 +1,35 @@
 'use client'
 
+import clsx from 'clsx'
 import Link from 'next/link'
-
 interface BreadcrumbProps {
-  pageName: string
-  homePage: string
+  label: string
+  href: string
+  activePage?: boolean
 }
 
-export default function Breadcrumb({ pageName, homePage }: BreadcrumbProps) {
+interface NewBreadcrumbProps {
+  pages: BreadcrumbProps[]
+}
+
+export default function Breadcrumb({ pages }: NewBreadcrumbProps) {
   return (
     <div className="pb-6 text-xl">
-      <Link
-        href={homePage}
-        className="1s text-blue-theme transition ease-in-out hover:text-blue-500"
-      >
-        {'Home > '}
-      </Link>
-      <span>{pageName}</span>
+      {pages.map((page, index) => {
+        return (
+          <span key={index}>
+            <Link
+              href={page.href}
+              className={clsx(
+                !page.activePage &&
+                  '1s text-blue-theme transition ease-in-out hover:text-blue-500',
+              )}
+            >
+              {page.label} {!page.activePage ? ' > ' : ''}
+            </Link>
+          </span>
+        )
+      })}
     </div>
   )
 }
