c722a88041bbab12bc902989aec206cf78988681
Nathan Soares <nathansoares809@gmail.com>
2024-01-15 15:51:05 -0300

    feat: use new Breadcrumb and button now opens a new page instead a modal

--- frontend/src/app/ui/dashboard/customers/customers-header.tsx
+++ frontend/src/app/ui/dashboard/customers/customers-header.tsx
@@ -1,35 +1,33 @@
 'use client'
 
-import { UserPlus } from '@phosphor-icons/react'
-
-import { useModal } from '@/app/hooks/useModal'
-
 import Breadcrumb from '@/app/ui/breadcrumb'
 import Button from '@/app/ui/button'
-import Modal from '@/app/ui/modal'
-import Form from '@/app/ui/form'
+
+import { UserPlus } from '@phosphor-icons/react'
+import Link from 'next/link'
 
 export default function CustomersHeader() {
-  const { isOpen, openModal, closeModal } = useModal()
-
   return (
     <>
-      <Breadcrumb pageName={'Customers'} homePage="/dashboard" />
+      <Breadcrumb
+        pages={[
+          { label: 'Home', href: '/dashboard' },
+          {
+            label: 'Customers',
+            href: '/dashboard/customers',
+            activePage: true,
+          },
+        ]}
+      />
       <div className="flex max-w-7xl justify-between">
         <h1 className="pb-6 pt-2 text-4xl font-bold">Customers</h1>
-        <Button.Root
-          className="m-2 flex items-center justify-center gap-2 border-blue-theme px-4 py-1 text-blue-theme hover:bg-blue-800 hover:text-white"
-          onClick={() => openModal()}
-        >
-          <Button.Icon icon={<UserPlus size={32} />} />
-          <Button.Label label="Create customer" />
-        </Button.Root>
+        <Link href="/dashboard/customers/create">
+          <Button.Root className="m-2 flex items-center justify-center gap-2 border-blue-theme px-4 py-1 text-blue-theme hover:bg-blue-800 hover:text-white">
+            <Button.Icon icon={<UserPlus size={32} />} />
+            <Button.Label label="Create customer" />
+          </Button.Root>
+        </Link>
       </div>
-      {isOpen && (
-        <Modal title="MyModal" onHide={() => closeModal()}>
-          <Form>Hello Modal</Form>
-        </Modal>
-      )}
     </>
   )
 }
