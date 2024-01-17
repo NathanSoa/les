bc00dd4c6a1dceb7b7e8fd10a45818afaa30d9fe
Nathan Soares <nathansoares809@gmail.com>
2024-01-17 09:04:17 -0300

    feat: change base page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,40 @@
-import { redirect } from 'next/navigation'
+import CompanyLogo from '@/app/ui/common/company-logo'
+import useTailwindColor from '@/app/hooks/useTailwindColor'
+import IconWithText from '@/app/ui/main/Icon'
+
+import { Heart, ShoppingCart, User } from '@phosphor-icons/react/dist/ssr'
 
 export default function Home() {
-  redirect('/login')
+  const getHexColor = useTailwindColor()
+
+  return (
+    <div className="mx-52 mt-8">
+      <header className="flex justify-between border-b-2 py-5">
+        <span className="flex items-center gap-1">
+          <CompanyLogo size={40} />
+          <span className="font-semibold text-blue-theme">MyCompany</span>
+        </span>
+        <div className="flex gap-8">
+          <IconWithText
+            color={getHexColor('gray-500')}
+            icon={User}
+            text="Perfil"
+            colorHoverTo="gray"
+          />
+          <IconWithText
+            color={getHexColor('gray-500')}
+            icon={Heart}
+            text="Favoritos"
+            colorHoverTo="gray"
+          />
+          <IconWithText
+            color={getHexColor('gray-500')}
+            icon={ShoppingCart}
+            text="Carrinho"
+            colorHoverTo="gray"
+          />
+        </div>
+      </header>
+    </div>
+  )
 }
