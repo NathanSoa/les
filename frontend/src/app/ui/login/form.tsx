fa771d87efeace3a0f83ca2341fde270a8704566
Nathan Soares <nathansoares809@gmail.com>
2024-01-22 16:47:31 -0300

    feat: add forget my password

--- frontend/src/app/ui/login/form.tsx
+++ frontend/src/app/ui/login/form.tsx
@@ -12,12 +12,12 @@ export default function FormLogin() {
         >
           E-mail
         </label>
-        <div className="flex h-10 w-80 bg-slate-100 focus-within:outline focus-within:outline-1 focus-within:outline-indigo-400">
+        <div className="flex h-10 bg-slate-100 focus-within:outline focus-within:outline-1 focus-within:outline-indigo-400">
           <div className="w-1 bg-blue-500" />
           <input
             type="text"
             id="email"
-            placeholder="E-mail"
+            placeholder="E-mail*"
             className="w-full bg-transparent pl-3 text-slate-600 outline-none placeholder:font-medium"
           />
         </div>
@@ -28,16 +28,19 @@ export default function FormLogin() {
           htmlFor="password"
           className="pb-2 text-sm font-normal text-zinc-500"
         >
-          Password
+          Senha
         </label>
-        <div className="flex h-10 w-80 bg-slate-100 focus-within:outline focus-within:outline-1 focus-within:outline-indigo-400">
+        <div className="flex h-10 bg-slate-100 focus-within:outline focus-within:outline-1 focus-within:outline-indigo-400">
           <div className="w-1 bg-blue-500" />
           <input
             type="password"
             id="password"
-            placeholder="Password"
+            placeholder="Senha*"
             className="w-full bg-transparent pl-3 text-slate-600 outline-none placeholder:font-medium"
           />
+        </div>
+        <div className="pt-2 text-right font-medium text-slate-500 hover:cursor-pointer hover:text-indigo-500">
+          Esqueci minha senha
         </div>
       </div>
       <Button.Root className="border-blue-theme text-blue-500 hover:bg-blue-500 hover:text-white">
